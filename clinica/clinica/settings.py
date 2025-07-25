5c62a341ecdfb5a8a74b47fb118ec71448bb3b38
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-24 23:51:19 -0500

    Agregar STATIC_ROOT para despliegue

--- clinica/clinica/settings.py
+++ clinica/clinica/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # importante para producción
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gestion/static'),
 ]
